67e9718711864dbb88e3915d619cab9a30270c85
salmingo <lxm@nao.cas.cn>
2021-04-16 14:36:51 +0800

    2021-04-16
    created

--- src/Parameter.hpp
+++ src/Parameter.hpp
@@ -8,7 +8,11 @@
 #define PARAMETER_H_
 
 #include <string>
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/foreach.hpp>
 
 using std::string;
 
@@ -19,6 +23,12 @@ struct Parameter {
 	bool useMotionAss;	/// 运动关联
 
 	/* 图像处理 */
+	// 预处理
+	string pathWork;	/// 工作路径. 处理结果存储在该目录下
+	string pathZero;	/// 合并后本底路径
+	string pathDark;	/// 合并后暗场路径
+	string pathFlat;	/// 合并后平场路径
+
 	// 背景算法
 
 	// 信号提取参数
@@ -27,6 +37,9 @@ struct Parameter {
 
 	// 目标输出参数
 
+	/* CMOS相机时间修正参数 */
+	int msPreClean;		/// 收到曝光指令后的芯片清空时间, 量纲: 毫秒
+	int msLinesShift;	/// 图像行转移时间, 量纲： 毫秒
 
 public:
 	/*!
@@ -34,6 +47,73 @@ public:
 	 * @param filepath 文件路径
 	 */
 	void Init(const string &filepath) {
+		using namespace boost::property_tree;
+		using namespace boost::property_tree::xml_parser;
+		using namespace boost::posix_time;
 
+		ptree nodes;
+		nodes.add("version", "0.1");
+		nodes.add("date", to_iso_extended_string(second_clock::universal_time()));
+
+		ptree& node1 = nodes.add("Function", "");
+		node1.add("Astrometry.<xmlattr>.Enable",        false);
+		node1.add("Photometry.<xmlattr>.Enable",        false);
+		node1.add("MotionAssociation.<xmlattr>.Enable", false);
+
+		ptree& node2 = nodes.add("ClockCorrect-for-CMOS",  "");
+		node2.add("<xmlattr>.PreClean",   0);
+		node2.add("<xmlattr>.LinesShift", 0);
+
+		ptree& node3 = nodes.add("PreProcess", "");
+		node3.add("Work.<xmlattr>.Dir",  "");
+		node3.add("ZERO.<xmlattr>.Path", "");
+		node3.add("DARK.<xmlattr>.Path", "");
+		node3.add("FLAT.<xmlattr>.Path", "");
+
+		xml_writer_settings<string> settings(' ', 4);
+		write_xml(filepath, nodes, std::locale(), settings);
+	}
+
+	/*!
+	 * @brief 加载配置参数文件
+	 * @param filepath 文件路径
+	 * @return
+	 * 文件加载结果
+	 */
+	bool Load(const string &filepath) {
+		try {
+			using namespace boost::property_tree;
+			using namespace boost::property_tree::xml_parser;
+
+			string str;
+			ptree nodes;
+
+			read_xml(filepath, nodes, trim_whitespace);
+			BOOST_FOREACH(ptree::value_type const &child, nodes.get_child("")) {
+				if (boost::iequals(child.first, "Function")) {
+					useAstrometry = child.second.get("Astrometry.<xmlattr>.Enable",        false);
+					usePhotometry = child.second.get("Photometry.<xmlattr>.Enable",        false);
+					useMotionAss  = child.second.get("MotionAssociation.<xmlattr>.Enable", false);
+				}
+				else if (boost::iequals(child.first, "Clock-Correct-For-CMOS")) {
+					msPreClean   = child.second.get("<xmlattr>.PreClean",   0);
+					msLinesShift = child.second.get("<xmlattr>.LinesShift", 0);
+				}
+				else if (boost::iequals(child.first, "PreProcess")) {
+					pathWork = child.second.get("Work.<xmlattr>.Dir",  "");
+					pathZero = child.second.get("ZERO.<xmlattr>.Path", "");
+					pathDark = child.second.get("DARK.<xmlattr>.Path", "");
+					pathFlat = child.second.get("FLAT.<xmlattr>.Path", "");
+				}
+			}
+
+			return true;
+		}
+		catch(boost::property_tree::xml_parser::xml_parser_error &ex) {
+			Init(filepath);
+			return false;
+		}
 	}
 };
+
+#endif
--- src/adips.cpp
+++ src/adips.cpp
@@ -9,11 +9,18 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <boost/filesystem.hpp>
 #include "Parameter.hpp"
+#include "GLog.h"
 
+///////////////////////////////////////////////////////////////////////
 using namespace std;
+using namespace boost::filesystem;
+
 typedef vector<string> strvec;
 
+GLog _gLog(stdout);
+///////////////////////////////////////////////////////////////////////
 /*!
  * @brief 显示使用说明
  */
@@ -24,9 +31,9 @@ void Usage() {
 	printf(" -h / --help    : print this help message\n");
 	printf(" -d / --default : generate default configuration file here\n");
 	printf(" -c / --config  : specify configuration file\n");
-	printf(" -Z / --zero    : combine bias images\n");
-	printf(" -D / --dark    : combine dark images\n");
-	printf(" -F / --flat    : combine flat images\n");
+	printf(" -Z / --zero    : combine bias images, result to be saved as ZERO.fits in WD\n");
+	printf(" -D / --dark    : combine dark images, result to be saved as DARK.fits in WD\n");
+	printf(" -F / --flat    : combine flat images, result to be saved as FLAT.fits in WD\n");
 }
 
 int main(int argc, char** argv) {
@@ -35,15 +42,14 @@ int main(int argc, char** argv) {
 		{ "help",    no_argument,       NULL, 'h' },
 		{ "default", no_argument,       NULL, 'd' },
 		{ "config",  required_argument, NULL, 'c' },
-		{ "zero",    required_argument, NULL, 'Z' },
-		{ "dark",    required_argument, NULL, 'D' },
-		{ "flat",    required_argument, NULL, 'F' },
+		{ "zero",    no_argument,       NULL, 'Z' },
+		{ "dark",    no_argument,       NULL, 'D' },
+		{ "flat",    no_argument,       NULL, 'F' },
 		{ NULL,      0,                 NULL,  0  }
 	};
-	char optstr[] = "hdc:Z:D:F:";
+	char optstr[] = "hdc:ZDF";
 	int ch, optndx;
 	bool combineZero(false), combineDark(false), combineFlat(false);
-	string pathParam;
 	Parameter param;
 
 	while ((ch = getopt_long(argc, argv, optstr, longopts, &optndx)) != -1) {
@@ -56,7 +62,10 @@ int main(int argc, char** argv) {
 			param.Init("adips.xml");
 			return -2;
 		case 'c':
-			pathParam = argv[optind - 1];
+			if (!param.Load(argv[optind - 1])) {
+				_gLog.Write(LOG_FAULT, "failed to load configuration file [%s]", argv[optind - 1]);
+				return -3;
+			}
 			break;
 		case 'Z':
 			combineZero = true;
@@ -75,12 +84,25 @@ int main(int argc, char** argv) {
 	argc -= optind;
 	argv += optind;
 	if (!argc) {
-		printf("requires FITS file which to be processed\n");
-		Usage();
-		return -3;
+		_gLog.Write(LOG_WARN, "require FITS file(s) which to be processed");
+		return -4;
 	}
 
 	/////////////////////////////////////////////////////////////////////////
+	strvec imgFiles;
+
+	for (int i = 0; i < argc; ++i) {
+		path filename = argv[i];
+		if (is_regular_file(filename) && filename.extension().string().rfind(".fit") != string::npos)
+			imgFiles.push_back(argv[i]);
+	}
+
+	if (combineZero || combineDark || combineFlat) {
+
+	}
+	else {
+
+	}
 	// 缓存文件
 	// 文件名增量排序
 	// 启动处理
