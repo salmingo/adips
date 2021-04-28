dec58c52c8307caae110436f500bedaaadc120e3
salmingo <lxm@nao.cas.cn>
2021-04-29 06:47:23 +0800

    210428
    minor

--- src/Parameter.hpp
+++ src/Parameter.hpp
@@ -18,28 +18,43 @@ using std::string;
 
 struct Parameter {
 	/* 启用功能 */
-	bool useAstrometry;	/// 天文定位
-	bool usePhotometry;	/// 测光
-	bool useMotionAss;	/// 运动关联
+	bool useAstrometry;		/// 天文定位
+	bool usePhotometry;		/// 测光
+	bool useMotionAss;		/// 运动关联
 
 	/* 图像处理 */
 	// 预处理
-	string pathWork;	/// 工作路径. 处理结果存储在该目录下
-	string pathZero;	/// 合并后本底路径
-	string pathDark;	/// 合并后暗场路径
-	string pathFlat;	/// 合并后平场路径
+	string pathWork;		/// 工作路径. 处理结果存储在该目录下
+	string pathZero;		/// 合并后本底路径
+	string pathDark;		/// 合并后暗场路径
+	string pathFlat;		/// 合并后平场路径
 
 	// 背景算法
+	bool backGlobal;		/// 使用全局背景
+	int backGridHor;		/// 背景网格宽度
+	int backGridVer;		/// 背景网格高度
+	int backFilterHor;		/// 背景X方向滤波宽度
+	int backFilterVer;		/// 背景Y方向滤波高度
 
 	// 信号提取参数
+	bool sigRemoveBadpix;	/// 剔除坏像素
+	bool sigUseFilter;		/// 检测信号前应用滤波算法
+	string sigPathFilter;	/// 用于检测信号的卷积核文件
+	float sigSNR;			/// 最小信噪比
 
-	// 信号聚合参数
+	// 信号测量参数
+	int blobPixMin;			/// 构成目标的最小像素数
+	int blobPixMax;			/// 构成目标的最大像素数. 0: 无限制
 
 	// 目标输出参数
 
 	/* CMOS相机时间修正参数 */
-	int msPreClean;		/// 收到曝光指令后的芯片清空时间, 量纲: 毫秒
-	int msLinesShift;	/// 图像行转移时间, 量纲： 毫秒
+	bool clockCorrect;		/// 使用时间修正
+	int msPreClean;			/// 收到曝光指令后的芯片清空时间, 量纲: 毫秒
+	int msLinesShift;		/// 图像行转移时间, 量纲： 毫秒
+
+	// 显示图形界面
+//	bool useGUI;			/// 启用GUI界面
 
 public:
 	/*!
@@ -60,15 +75,27 @@ public:
 		node1.add("Photometry.<xmlattr>.Enable",        false);
 		node1.add("MotionAssociation.<xmlattr>.Enable", false);
 
-		ptree& node2 = nodes.add("ClockCorrect-for-CMOS",  "");
-		node2.add("<xmlattr>.PreClean",   0);
-		node2.add("<xmlattr>.LinesShift", 0);
+		ptree& node2 = nodes.add("PreProcess", "");
+		node2.add("Work.<xmlattr>.Dir",  "");
+		node2.add("ZERO.<xmlattr>.Path", "");
+		node2.add("DARK.<xmlattr>.Path", "");
+		node2.add("FLAT.<xmlattr>.Path", "");
 
-		ptree& node3 = nodes.add("PreProcess", "");
-		node3.add("Work.<xmlattr>.Dir",  "");
-		node3.add("ZERO.<xmlattr>.Path", "");
-		node3.add("DARK.<xmlattr>.Path", "");
-		node3.add("FLAT.<xmlattr>.Path", "");
+		ptree& node3 = nodes.add("BackGround", "");
+		node3.add("Global.<xmlattr>.Enable", false);
+		node3.add("Grid.<xmlattr>.Horizontal", 64);
+		node3.add("Grid.<xmlattr>.Vertical", 64);
+		node3.add("Filter.<xmlattr>.Horizontal", 3);
+		node3.add("Filter.<xmlattr>.Vertical", 3);
+
+		ptree& node5 = nodes.add("BlobMesurement", "");
+		node5.add("PixelNumber.<xmlattr>.Minimum", 1);
+		node5.add("PixelNumber.<xmlattr>.Maximum", 4);
+
+		ptree& node7 = nodes.add("ClockCorrect-for-CMOS",  "");
+		node7.add("<xmlattr>.Enable",     false);
+		node7.add("<xmlattr>.PreClean",   0);
+		node7.add("<xmlattr>.LinesShift", 0);
 
 		xml_writer_settings<string> settings(' ', 4);
 		write_xml(filepath, nodes, std::locale(), settings);
@@ -95,22 +122,37 @@ public:
 					usePhotometry = child.second.get("Photometry.<xmlattr>.Enable",        false);
 					useMotionAss  = child.second.get("MotionAssociation.<xmlattr>.Enable", false);
 				}
-				else if (boost::iequals(child.first, "Clock-Correct-For-CMOS")) {
-					msPreClean   = child.second.get("<xmlattr>.PreClean",   0);
-					msLinesShift = child.second.get("<xmlattr>.LinesShift", 0);
-				}
 				else if (boost::iequals(child.first, "PreProcess")) {
 					pathWork = child.second.get("Work.<xmlattr>.Dir",  "");
 					pathZero = child.second.get("ZERO.<xmlattr>.Path", "");
 					pathDark = child.second.get("DARK.<xmlattr>.Path", "");
 					pathFlat = child.second.get("FLAT.<xmlattr>.Path", "");
 				}
+				else if (boost::iequals(child.first, "BackGround")) {
+					backGlobal     = child.second.get("Global.<xmlattr>.Enable",     false);
+					backGridHor    = child.second.get("Grid.<xmlattr>.Horizontal",   32);
+					backGridVer    = child.second.get("Grid.<xmlattr>.Vertical",     32);
+					backFilterHor  = child.second.get("Filter.<xmlattr>.Horizontal", 3);
+					backFilterVer  = child.second.get("Filter.<xmlattr>.Vertical",   3);
+				}
+				else if (boost::iequals(child.first, "BlobMesurement")) {
+					blobPixMin = child.second.get("PixelNumber.<xmlattr>.Minimum",  1);
+					blobPixMax = child.second.get("PixelNumber.<xmlattr>.Maximum",  0);
+
+					if (blobPixMin <= 0) blobPixMin = 1;
+					if (blobPixMax <= 0) blobPixMax = 0;
+				}
+
+				else if (boost::iequals(child.first, "Clock-Correct-For-CMOS")) {
+					clockCorrect = child.second.get("<xmlattr>.Enable",     false);
+					msPreClean   = child.second.get("<xmlattr>.PreClean",   0);
+					msLinesShift = child.second.get("<xmlattr>.LinesShift", 0);
+				}
 			}
 
 			return true;
 		}
 		catch(xml_parser_error &ex) {
-			Init(filepath);
 			return false;
 		}
 	}
